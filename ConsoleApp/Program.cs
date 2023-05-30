bec27210961701f564096e6960dfc5f257dcc8cc
d.kistanov <d.kistanov@parcsis.org>
2023-05-30 12:58:15 +0400

    init

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,26 +9,50 @@ db.Database.EnsureCreated();
 
 var request = new Request
 {
-    Info = new RequestInfo { Text = "AnyText" }
+    Info = new RequestInfo { Text = "Any" }
 };
 
 db.Add(request);
 db.SaveChanges();
 
-var resultByEf = db.Requests.Single(x => x.Id == request.Id);
+var config = new MapperConfiguration(cfg =>
+{
+    cfg.CreateMap<Request, RequestDto>();
+    cfg.CreateMap<RequestInfo, RequestInfoDto>();
+});
 
-Console.WriteLine($"By original request: {resultByEf.Info?.Text}");
-// By original request: AnyText
-
-var config = new MapperConfiguration(cfg => cfg.CreateMap<Request, RequestDto>());
-
-var resultByAutoMapper = db.Requests
-    .AsNoTracking()
+var result = db.Requests
     .ProjectTo<RequestDto>(config)
     .Single(x => x.Id == request.Id);
 
-Console.WriteLine($"By automapper request: {resultByAutoMapper.Info?.Text}");
-// By automapper request: AnyText ??????
+Console.WriteLine(result.Info?.Text != null);
+// Write FALSE, expected TRUE
+
+
+Console.WriteLine(db.Requests.ProjectTo<RequestDto>(config).Expression);
+/*		
+db.Requests.ProjectTo<RequestDto>(config).Expression	
+{[Microsoft.EntityFrameworkCore.Query.EntityQueryRootExpression].
+Select(dtoRequest => new RequestDto() 
+{
+ 	Id = dtoRequest.Id, 
+ 	Info = IIF((dtoRequest.Info == default(Object)), null, new RequestInfoDto() 
+ 	{
+ 		Text = dtoRequest.Info.Text, 
+ 		Number = dtoRequest.Info.Number
+ 	})})}	System.Linq.Expressions.Expression {System.Linq.Expressions.MethodCallExpression2}
+*/
+
+Console.WriteLine(db.Requests.ProjectTo<RequestDto>(config).ToQueryString());
+/* 
+SELECT [r].[Id], CASE
+WHEN([r].[Info_Number] IS NULL) OR ([r].[Info_Text] IS NULL) THEN CAST(1 AS bit)
+    ELSE CAST(0 AS bit)
+END, [r].[Info_Text], [r].[Info_Number]
+FROM[Requests] AS[r]
+
+Why operator is "OR"?
+*/
 
 Console.ReadKey();
 
@@ -39,7 +63,7 @@ public class DomainContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql(@"Server=localhost;Port=5432;Database=DomainContext;User ID=postgres;Password=password;");
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=DomainContext;Trusted_Connection=True");
     }
 }
 
@@ -59,7 +83,7 @@ public class RequestInfo
 public class RequestDto
 {
     public Guid Id { get; set; }
-    public RequestInfo Info { get; set; }
+    public RequestInfoDto Info { get; set; }
 }
 
 public class RequestInfoDto
